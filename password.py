fed0f337cf81899bb56452c9f24ca0fb053ff2a3
Ashley Mosoti <ninahb746@gmail.com>
2020-06-27 20:38:39 +0300

    Use init method to create new instances

--- password.py
+++ password.py
@@ -2,4 +2,11 @@ class password:
     """
     Class that creates new passwords
     """
-    
+
+def __init__(self,username,phone_number,password):
+    '''
+    __init__ method helps you create your credentials.
+
+    self.username = username
+    self.phone_number = number
+    self.password = password
